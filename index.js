ba9f1d783280f2129c01b456e2eab9d888bc2c55
Raphael Nobre <nobre.roots@gmail.com>
2023-05-29 21:30:52 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const openGit = () => window.open('https://github.com/raphaelnb')
+
+const openLinkedin = () => window.open('https://www.linkedin.com/in/raphaelnbrandao/')
+
+const mailTo = () => window.location.href = 'mailto:raphael.brandao@engnetconsultoria.com.br'
